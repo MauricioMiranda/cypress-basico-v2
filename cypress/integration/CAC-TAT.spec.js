7a202c7fb339c44684b09937334f972c1d652bd2
Mauricio Miranda <mauriciodesouzamiranda@gmail.com>
2023-02-16 14:49:00 -0300

    Atualizando teste com nova funcionalidade do Cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -52,6 +52,14 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('have.value', '')
     })
 
+      Cypress._.times(3, function(){
+      it.only('campo testefone continua vazio quando preenchido com valor não-numérico', function(){
+        cy.get('#phone')
+          .type('abcdefghij')
+          .should('have.value', '')
+      })
+    })
+
     it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
 
       cy.clock()
