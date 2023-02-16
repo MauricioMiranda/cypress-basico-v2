2777c02913ef309c91776828bd4fe276e33e121a
Mauricio Miranda <mauriciodesouzamiranda@gmail.com>
2023-02-16 16:24:04 -0300

    Finalizando o curso basico de Cypress

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -222,13 +222,31 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         .should('not.be.visible')
     })
 
-    it.only('preencher a area de texto usando o comando invoke', function(){
+    it('preencher a area de texto usando o comando invoke', function(){
       const longText = Cypress._.repeat('0123456789', 20)
-      
+
       cy.get('#open-text-area')
         .invoke('val', longText)
         .should('have.value', longText)
     })
 
+    it('faz uma requisição HTTP', function(){
+      cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+        .should(function(response){
+          const { status, statusText, body } = response
+          expect(status).to.equal(200)
+          expect(statusText).to.equal('OK')
+          expect(body).to.include('CAC TAT')
+        })
+    })
+    it.only('encontrar o gato escondido', function(){
+      cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
+      cy.get('#title')
+        .invoke('text', 'CAT TAT')
+      cy.get('#subtitle')
+        .invoke('text', 'Eu ❤️ cats!')
+    })
 
   })
